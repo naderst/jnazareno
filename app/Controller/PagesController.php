58035a9cfff4da6b29057f2c105b83f1ddd1a352
Nader Abu Fakhr <naderst@gmail.com>
2014-07-24 16:54:31 -0430

    Añadida estadística de matrimonios

--- app/Controller/PagesController.php
+++ app/Controller/PagesController.php
@@ -34,11 +34,11 @@ class PagesController extends AppController {
 	public $name = 'Pages';
 	public $uses = array();
 
-    function beforeFilter() {
-        if(!$this->Auth->loggedIn())
-            $this->redirect('/usuarios/login');
-    }
-    
+	function beforeFilter() {
+		if(!$this->Auth->loggedIn())
+			$this->redirect('/usuarios/login');
+	}
+	
 	function display() {
 		$this->render('home');
 	}
@@ -58,114 +58,129 @@ class PagesController extends AppController {
 
 		return $edad;
 	}
-    
-    function estadisticas($tipo='del', $param1='dia', $param2=null) {
-        Configure::write('debug',0);
+	
+	function estadisticas($tipo='del', $param1='dia', $param2=null) {
+		Configure::write('debug',0);
 
-        $this->autoRender = false;
-        $this->response->type('application/pdf');
-        $this->loadModel('Configuracion');
-        $this->loadModel('Bautizo');
+		$this->autoRender = false;
+		$this->response->type('application/pdf');
+		$this->loadModel('Configuracion');
+		$this->loadModel('Bautizo');
+		$this->loadModel('Matrimonio');
 
-        if($tipo == 'del') {
-            if($param1 == 'dia') {
-                $time = time();
-                $hoy = date('d', $time) . ' de ' . parent::month2string(date('m',$time)) . ' de ' . date('Y', $time);
-                $title = 'Estadística del ' . $hoy;
-                $totalbautizos = $this->Bautizo->find('count', array(
-                    'conditions' => array('Bautizo.fecha' => date('d/m/Y', $time))
-                ));
-            } elseif($param1 == 'mes') {
-                $time = time();
-                $fecha = parent::month2string(date('m',$time)) . ' de ' . date('Y', $time);
-                $title = 'Estadística del mes de ' . $fecha;
-                $totalbautizos = $this->Bautizo->find('count', array(
-                    'conditions' => array('MONTH(STR_TO_DATE(REPLACE(Bautizo.fecha, \'/\', \'.\'), \'%d.%m.%Y\'))' => date('n', $time))
-                ));
-            } elseif($param1 == 'ano') {
-                $time = time();
-                $fecha = date('Y', $time);
-                $title = 'Estadística del año ' . $fecha;
-                $totalbautizos = $this->Bautizo->find('count', array(
-                    'conditions' => array('YEAR(STR_TO_DATE(REPLACE(Bautizo.fecha, \'/\', \'.\'), \'%d.%m.%Y\'))' => date('y', $time))
-                ));
-            } elseif($param1 == 'todos') {
-                $title = 'Estadística de todos los tiempos';
-                $totalbautizos = $this->Bautizo->find('count');
-            } else {
-                return;
-            }
-        } elseif($tipo == 'rango') {
-            $_param1 = strtotime($param1);
-            $_param2 = strtotime($param2);
+		if($tipo == 'del') {
+			if($param1 == 'dia') {
+				$time = time();
+				$hoy = date('d', $time) . ' de ' . parent::month2string(date('m',$time)) . ' de ' . date('Y', $time);
+				$title = 'Estadística del ' . $hoy;
+				$totalbautizos = $this->Bautizo->find('count', array(
+					'conditions' => array('Bautizo.fecha' => date('d/m/Y', $time))
+				));
+				$totalmatrimonios = $this->Matrimonio->find('count', array(
+					'conditions' => array('Matrimonio.fecha' => date('d/m/Y', $time))
+				));
+			} elseif($param1 == 'mes') {
+				$time = time();
+				$fecha = parent::month2string(date('m',$time)) . ' de ' . date('Y', $time);
+				$title = 'Estadística del mes de ' . $fecha;
+				$totalbautizos = $this->Bautizo->find('count', array(
+					'conditions' => array('MONTH(STR_TO_DATE(REPLACE(Bautizo.fecha, \'/\', \'.\'), \'%d.%m.%Y\'))' => date('n', $time))
+				));
+				$totalmatrimonios = $this->Matrimonio->find('count', array(
+					'conditions' => array('MONTH(STR_TO_DATE(REPLACE(Matrimonio.fecha, \'/\', \'.\'), \'%d.%m.%Y\'))' => date('n', $time))
+				));
+			} elseif($param1 == 'ano') {
+				$time = time();
+				$fecha = date('Y', $time);
+				$title = 'Estadística del año ' . $fecha;
+				$totalbautizos = $this->Bautizo->find('count', array(
+					'conditions' => array('YEAR(STR_TO_DATE(REPLACE(Bautizo.fecha, \'/\', \'.\'), \'%d.%m.%Y\'))' => $fecha)
+				));
+				$totalmatrimonios = $this->Matrimonio->find('count', array(
+					'conditions' => array('YEAR(STR_TO_DATE(REPLACE(Matrimonio.fecha, \'/\', \'.\'), \'%d.%m.%Y\'))' => $fecha)
+				));
+			} elseif($param1 == 'todos') {
+				$title = 'Estadística de todos los tiempos';
+				$totalbautizos = $this->Bautizo->find('count');
+				$totalmatrimonios = $this->Matrimonio->find('count');
+			} else {
+				return;
+			}
+		} elseif($tipo == 'rango') {
+			$_param1 = strtotime($param1);
+			$_param2 = strtotime($param2);
 
-            $totalbautizos = $this->Bautizo->find('count', array(
-                'conditions' => array('STR_TO_DATE(REPLACE(Bautizo.fecha, \'/\', \'.\'), \'%d.%m.%Y\') BETWEEN ? AND ?' => array(date('d.m.Y', $_param1), date('d.m.Y', $_param2)))
-            ));
+			$totalbautizos = $this->Bautizo->find('count', array(
+				'conditions' => array('STR_TO_DATE(REPLACE(Bautizo.fecha, \'/\', \'.\'), \'%d.%m.%Y\') BETWEEN ? AND ?' => array(date('d.m.Y', $_param1), date('d.m.Y', $_param2)))
+			));
 
-            $diadesde = date('d', $_param1);
-            $mesdesde = parent::month2string(date('m', $_param1));
-            $anodesde = date('Y', $_param1);
+			$totalmatrimonios = $this->Matrimonio->find('count', array(
+				'conditions' => array('STR_TO_DATE(REPLACE(Matrimonio.fecha, \'/\', \'.\'), \'%d.%m.%Y\') BETWEEN ? AND ?' => array(date('d.m.Y', $_param1), date('d.m.Y', $_param2)))
+			));
 
-            $diahasta = date('d', $_param2);
-            $meshasta = parent::month2string(date('m', $_param2));
-            $anohasta = date('Y', $_param2);
+			$diadesde = date('d', $_param1);
+			$mesdesde = parent::month2string(date('m', $_param1));
+			$anodesde = date('Y', $_param1);
 
-            $title = 'Estadística ';
+			$diahasta = date('d', $_param2);
+			$meshasta = parent::month2string(date('m', $_param2));
+			$anohasta = date('Y', $_param2);
 
-            if($anodesde == $anohasta && $mesdesde == $meshasta && $diadesde == $diahasta) {
-                $title .= 'del ' . $diadesde . ' de ' . $mesdesde . ' de ' . $anodesde;
-            } elseif($anodesde == $anohasta && $mesdesde == $meshasta) {
-                $title .= 'del ' . $diadesde . ' hasta ' . $diahasta . ' de ' . $mesdesde . ' de ' . $anodesde;
-            } elseif($anodesde == $anohasta && $mesdesde != $meshasta) {
-                $title .= 'del ' .$diadesde . ' de ' . $mesdesde . ' hasta el ' . $diahasta . ' de ' . $meshasta . ' de ' . $anodesde;
-            } else {
-                $title .= 'del ' .$diadesde . ' de ' . $mesdesde . ' de ' . $anodesde . ' hasta el ' . $diahasta . ' de ' . $meshasta . ' de ' . $anohasta;
-            }
-        } else {
-            return;
-        }
+			$title = 'Estadística ';
 
-        $url = 'http://' . $_SERVER['HTTP_HOST'] . Router::url('/graficos/bautizosedades/' . $tipo . '/' . $param1 . '/' . $param2);
-        $url2 = 'http://' . $_SERVER['HTTP_HOST'] . Router::url('/graficos/bautizosgenero/' . $tipo . '/' . $param1 . '/' . $param2);
+			if($anodesde == $anohasta && $mesdesde == $meshasta && $diadesde == $diahasta) {
+				$title .= 'del ' . $diadesde . ' de ' . $mesdesde . ' de ' . $anodesde;
+			} elseif($anodesde == $anohasta && $mesdesde == $meshasta) {
+				$title .= 'del ' . $diadesde . ' hasta ' . $diahasta . ' de ' . $mesdesde . ' de ' . $anodesde;
+			} elseif($anodesde == $anohasta && $mesdesde != $meshasta) {
+				$title .= 'del ' .$diadesde . ' de ' . $mesdesde . ' hasta el ' . $diahasta . ' de ' . $meshasta . ' de ' . $anodesde;
+			} else {
+				$title .= 'del ' .$diadesde . ' de ' . $mesdesde . ' de ' . $anodesde . ' hasta el ' . $diahasta . ' de ' . $meshasta . ' de ' . $anohasta;
+			}
+		} else {
+			return;
+		}
 
-        $time = time();
-        $presbitero = '';
+		$url = 'http://' . $_SERVER['HTTP_HOST'] . Router::url('/graficos/bautizosedades/' . $tipo . '/' . $param1 . '/' . $param2);
+		$url2 = 'http://' . $_SERVER['HTTP_HOST'] . Router::url('/graficos/bautizosgenero/' . $tipo . '/' . $param1 . '/' . $param2);
 
-        $config = $this->Configuracion->find('all');
+		$time = time();
+		$presbitero = '';
 
-        foreach($config as $e)
-            if($e['Configuracion']['parametro'] == 'presbitero') {
-                $presbitero = $e['Configuracion']['valor'];
-                break;
-            }
-        $html = '<div class="titulo">' . parent::strtoupper_utf8($title) . '</div><br><br>';
-        $html .= '<p align="center"><img src="'.$url.'"><img src="' . $url2 . '"></p>';
-        $html .= '<br><table id="sacramentos"><tr><th>Sacramento</th><th>Total</th></tr><tr>
-            <td>Bautizos</td>
-            <td class="total">'.$totalbautizos.'</td>
-        </tr>
-            <tr>
-            <td>Confirmaciones</td>
-            <td class="total">0</td>
-            </tr>
-            <tr>
-            <td>Comuniones</td>
-            <td class="total">0</td>
-            </tr>
-            <tr>
-            <td>Matrimonios</td>
-            <td class="total">0</td>
-            </tr>
-        </table><br><br>';
-        $html .= 'PUERTO ORDAZ, ' . date('d', $time) . ' DE ' . strtoupper(parent::month2string(date('m', $time))) .' DE ' . date('Y', $time) . '<br>Doy Fe.<br><br><div style="text-align:center;"><b>PRESBITERO ' . parent::strtoupper_utf8($presbitero) . '</b><br><br><br><br><br><b>PÁRROCO</b></div>';
-        
-        $mpdf = new mPDF('BLANK', 'Letter', '11', 'Arial', 10, 10, 35, 5, 3, 3);
-        $mpdf->writeHTML('.total { text-align:center; } #sacramentos tr:nth-child(odd) {background-color: #F2F2EA} #sacramentos th {background-color: #9b59b6;text-align: center;color: #fff;text-shadow: 1px 1px 2px #000;} #sacramentos { width:400px; margin:auto; border-collapse: collapse; } #sacramentos, #sacramentos td, #sacramentos th { border: 1px solid #EBEBE1 }  #sacramentos td, #sacramentos th { padding: 10px; } #logo { text-align:center } #footer { text-align: center; font-size:12px; border-top: 1px solid #666; padding-top: 5px } .titulo { text-align: center; font-size: 17px; font-weight: bold; }', 1);
-        $mpdf->setHTMLHeader('<div id="logo"><img src="http://localhost' . Router::url('/img/logo.png') . '"></div>');
-        $mpdf->setHTMLFooter('<div id="footer">Urbanización Villa Brasil, Final Senda Curitiva. Puerto Ordaz, Estado Bolívar.<br><b>Telf.:</b> (0286) 923.27.85</div>');
-        $mpdf->WriteHTML($html, 2);
-        $mpdf->debug=true;
-        $mpdf->Output($title, 'I');
-    }
+		$config = $this->Configuracion->find('all');
+
+		foreach($config as $e)
+			if($e['Configuracion']['parametro'] == 'presbitero') {
+				$presbitero = $e['Configuracion']['valor'];
+				break;
+			}
+		$html = '<div class="titulo">' . parent::strtoupper_utf8($title) . '</div><br><br>';
+		$html .= '<p align="center"><img src="'.$url.'"><img src="' . $url2 . '"></p>';
+		$html .= '<br><table id="sacramentos"><tr><th>Sacramento</th><th>Total</th></tr><tr>
+			<td>Bautizos</td>
+			<td class="total">'.$totalbautizos.'</td>
+		</tr>
+			<tr>
+			<td>Confirmaciones</td>
+			<td class="total">0</td>
+			</tr>
+			<tr>
+			<td>Comuniones</td>
+			<td class="total">0</td>
+			</tr>
+			<tr>
+			<td>Matrimonios</td>
+			<td class="total">'.$totalmatrimonios.'</td>
+			</tr>
+		</table><br><br>';
+		$html .= 'PUERTO ORDAZ, ' . date('d', $time) . ' DE ' . strtoupper(parent::month2string(date('m', $time))) .' DE ' . date('Y', $time) . '<br>Doy Fe.<br><br><div style="text-align:center;"><b>PRESBITERO ' . parent::strtoupper_utf8($presbitero) . '</b><br><br><br><br><br><b>PÁRROCO</b></div>';
+		
+		$mpdf = new mPDF('BLANK', 'Letter', '11', 'Arial', 10, 10, 35, 5, 3, 3);
+		$mpdf->writeHTML('.total { text-align:center; } #sacramentos tr:nth-child(odd) {background-color: #F2F2EA} #sacramentos th {background-color: #9b59b6;text-align: center;color: #fff;text-shadow: 1px 1px 2px #000;} #sacramentos { width:400px; margin:auto; border-collapse: collapse; } #sacramentos, #sacramentos td, #sacramentos th { border: 1px solid #EBEBE1 }  #sacramentos td, #sacramentos th { padding: 10px; } #logo { text-align:center } #footer { text-align: center; font-size:12px; border-top: 1px solid #666; padding-top: 5px } .titulo { text-align: center; font-size: 17px; font-weight: bold; }', 1);
+		$mpdf->setHTMLHeader('<div id="logo"><img src="http://localhost' . Router::url('/img/logo.png') . '"></div>');
+		$mpdf->setHTMLFooter('<div id="footer">Urbanización Villa Brasil, Final Senda Curitiva. Puerto Ordaz, Estado Bolívar.<br><b>Telf.:</b> (0286) 923.27.85</div>');
+		$mpdf->WriteHTML($html, 2);
+		$mpdf->debug=true;
+		$mpdf->Output($title, 'I');
+	}
 }
