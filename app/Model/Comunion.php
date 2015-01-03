23a16e29e56dedf7fcdd64054386c1d96ce20bb0
Nader Abu Fakhr <naderst@gmail.com>
2015-01-02 23:38:29 -0430

    Validación en Comunión y Confirmación

--- app/Model/Comunion.php
+++ app/Model/Comunion.php
@@ -1,5 +1,31 @@
 <?php
 class Comunion extends AppModel {
   public $useTable = 'comuniones';
+  public $validate = array(
+    'fecha' => array(
+      'rule' => array('date', 'dmy'),
+      'message' => 'El formato de la fecha es inválido debe ser dd/mm/aaaa'
+    ),
+    'nombres' => array(
+      'rule' => '/^[A-Za-zá-úÁ-ÚñÑ ]{2,50}$/',
+      'message' => 'Los nombres deben tener entre 2 y 50 caracteres'
+    ),
+    'apellidos' => array(
+      'rule' => '/^[A-Za-zá-úÁ-ÚñÑ ]{2,50}$/',
+      'message' => 'Los apellidos deben tener entre 2 y 50 caracteres'
+    ),
+    'padre' => array(
+      'rule' => '/^[A-Za-zá-úÁ-ÚñÑ ]{2,100}$/',
+      'message' => 'El nombre completo del padre debe tener entre 5 y 100 caracteres'
+    ),
+    'madre' => array(
+      'rule' => '/^[A-Za-zá-úÁ-ÚñÑ ]{2,100}$/',
+      'message' => 'El nombre completo de la madre debe tener entre 5 y 100 caracteres'
+    ),
+    'ministro' => array(
+      'rule' => array('between', 5, 50),
+      'message' => 'El nombre del ministro debe tener entre 5 y 50 caracteres'
+    )
+  );
 }
 ?>
--- app/Model/Confirmacion.php
+++ app/Model/Confirmacion.php
@@ -1,5 +1,37 @@
 <?php
 class Confirmacion extends AppModel {
   public $useTable = 'confirmaciones';
+  public $validate = array(
+    'fecha' => array(
+      'rule' => array('date', 'dmy'),
+      'message' => 'El formato de la fecha es inválido debe ser dd/mm/aaaa'
+    ),
+    'nombres' => array(
+      'rule' => '/^[A-Za-zá-úÁ-ÚñÑ ]{2,50}$/',
+      'message' => 'Los nombres deben tener entre 2 y 50 caracteres'
+    ),
+    'apellidos' => array(
+      'rule' => '/^[A-Za-zá-úÁ-ÚñÑ ]{2,50}$/',
+      'message' => 'Los apellidos deben tener entre 2 y 50 caracteres'
+    ),
+    'padre' => array(
+      'rule' => '/^[A-Za-zá-úÁ-ÚñÑ ]{2,100}$/',
+      'message' => 'El nombre completo del padre debe tener entre 5 y 100 caracteres'
+    ),
+    'madre' => array(
+      'rule' => '/^[A-Za-zá-úÁ-ÚñÑ ]{2,100}$/',
+      'message' => 'El nombre completo de la madre debe tener entre 5 y 100 caracteres'
+    ),
+    'padrino' => array(
+      'rule' => array('between', 5, 100),
+      'message' => 'El nombre completo del padrino / madrina debe tener entre 5 y 100 caracteres',
+      'required' => false,
+      'allowEmpty' => true
+    ),
+    'ministro' => array(
+      'rule' => array('between', 5, 50),
+      'message' => 'El nombre del ministro debe tener entre 5 y 50 caracteres'
+    )
+  );
 }
 ?>
